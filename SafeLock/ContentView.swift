733b99d6983573aebd2c33f6422221234be1cba2
Divyesh Canopas <divyesh.v@canopas.com>
2024-04-23 08:56:18 +0530

    Initial Commit

---
+++ SafeLock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SafeLock
+//
+//  Created by Divyesh Vekariya on 23/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SafeLock/SafeLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeLockApp.swift
+//  SafeLock
+//
+//  Created by Divyesh Vekariya on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SafeLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
